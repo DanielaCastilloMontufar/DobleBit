1bdfe2c8cfe074ac7c7f30e2f910cb2500154471
DanielaCastilloMontufar <82785812+DanielaCastilloMontufar@users.noreply.github.com>
2023-08-08 22:53:21 -0500

    Archivos sin carpeta

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			Comfortaa: ['Comfortaa', 'sans-serif'],
+			'Montserrat': ['Montserrat','sans-serif'],
+			'FjallaOne': ['FjallaOne','sans-serif'],
+			'Poppins': ['Poppins','sans-serif'],
+			'Raleway': ['Raleway', 'sans-serif'],
+			'MontserratE': ['MontserratE', 'sans-serif']
+		},
+		colors: {
+				'green': '#18DB59',
+				'black': '#000000',
+				'white': '#ffffff',
+				'bluek': '#17171f',
+				'new': '#002D95',
+				'gray': '#808080',
+				'hello': '#e5e7eb',
+				transparent: 'transparent'
+		},
+
+	},
+	plugins: [],
+}
